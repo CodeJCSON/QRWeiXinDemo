c6d9fc3d187d44978f3f244fb8c12c67e4eacc98
CodeJCSON <2334497007@qq.com>
2017-02-20 08:55:12 +0800

    QRWeiXinDemo
    QRWeiXinDemo微信扫码

---
+++ Example/build/Pods.build/Debug-iphoneos/DDQRCode.build/DerivedSources/DDQRCode_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char DDQRCodeVersionString[];
+ extern const double DDQRCodeVersionNumber;
+
+ const unsigned char DDQRCodeVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DDQRCode  PROJECT:Pods-1" "\n";
+ const double DDQRCodeVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Example/build/Pods.build/Debug-iphoneos/Pods-DDQRCode_Example.build/DerivedSources/Pods_DDQRCode_Example_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_DDQRCode_ExampleVersionString[];
+ extern const double Pods_DDQRCode_ExampleVersionNumber;
+
+ const unsigned char Pods_DDQRCode_ExampleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_DDQRCode_Example  PROJECT:Pods-1" "\n";
+ const double Pods_DDQRCode_ExampleVersionNumber __attribute__ ((used)) = (double)1.;
